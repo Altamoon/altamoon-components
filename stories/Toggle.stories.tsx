ad9738300a86a1acbdae6c8ef30c868b005a75ec
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-12-24 14:26:54 +0200

    Add Styrobook and Toggle component

---
+++ stories/Toggle.stories.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Toggle from '../components/Toggle';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Altamoon/Toggle',
+  component: Toggle,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = ({
+  id, checkedLabel, uncheckedLabel, className,
+}: Parameters<typeof Toggle>[0]) => {
+  const [isChecked, onChange] = useState(false);
+
+  return (
+    <Toggle
+      id={id}
+      checkedLabel={checkedLabel}
+      uncheckedLabel={uncheckedLabel}
+      className={className}
+      isChecked={isChecked}
+      onChange={onChange}
+    />
+  );
+};
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  checkedLabel: 'Checked',
+  uncheckedLabel: 'Unchecked',
+  className: '',
+  id: 'toggle',
+};
